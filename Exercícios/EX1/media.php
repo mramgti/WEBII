2edc6df01a357e7d6ac0c7dd79ccc77ad2a1a67e
Maycon Ramos <maycon_marchitto@hotmail.com>
2026-03-12 22:00:19 -0300

    Material de WEB II

---
+++ Exercícios/EX1/media.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média Ponderada</title>
+</head>
+<body>
+    <?php
+     $nota1 = str_replace(",",".",$_POST["txtNota1"]);
+     $peso1 = str_replace(",",".",$_POST["txtPeso1"]);
+     $nota2 = str_replace(",",".",$_POST["txtNota2"]);
+     $peso2 = str_replace(",",".",$_POST["txtPeso2"]);
+     $nota3 = str_replace(",",".",$_POST["txtNota3"]);
+     $peso3 = str_replace(",",".",$_POST["txtPeso3"]);
+
+     $media = (($nota1*$peso1) + ($nota2*$peso2) + ($nota3*$peso3)) / ($peso1+$peso2+$peso3);
+     
+     //echo "A média ponderada é:" .$media;
+     echo "<p class='result'>".number_format($media, 1, "," ,".")."</p>";
+     
+     
+    ?> 
+</body>
+</html>
